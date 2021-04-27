3feddd5065c20bdb0b574113b59c8766fae59311
Richard Basdeo <basdeorichard@gmail.com>
2021-04-27 16:37:48 -0400

    Displays running sets using tableView. - Richard

--- My Gym Tracker/Controller/RunningViewController.swift
+++ My Gym Tracker/Controller/RunningViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class RunningViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
     //Connect viewController to model
     var controller = RunControl()
     
@@ -16,16 +18,54 @@ class RunningViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         controller.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
         //Load all of users's running workouts saved
         controller.loadRunning()
     }
 }
 
+extension RunningViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        controller.allRunningSets.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RunningCell") as! RunningTableViewCell
+        
+        guard let theDate = controller.allRunningSets[indexPath.row].date,
+              let theDuration = controller.allRunningSets[indexPath.row].duration else {
+            return cell
+        }
+        
+        cell.setDate(date: theDate)
+        cell.setSpeed(speed: controller.allRunningSets[indexPath.row].speed)
+        cell.setDuratation(duration: theDuration)
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.frame.height/5
+    }
+    
+    
+    
+}
+
 extension RunningViewController: runProtocol {
     func updateUI() {
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
     
     func error(error: Error) {
+        
+        print(error)
     }
 }
---
+++ My Gym Tracker/View/Cells/RunningTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  RunningTableViewCell.swift
+//  My Gym Tracker
+//
+//  Created by Richard Basdeo on 4/27/21.
+//
+
+import UIKit
+
+class RunningTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setDate(date: String){
+        
+        dateLabel.text = date
+        
+    }
+    
+    func setDuratation(duration: String) {
+        
+        durationLabel.text = "Duration: \(duration)"
+        
+    }
+    
+    func setSpeed(speed: Float){
+        
+        let stringSpeed = String(speed)
+        speedLabel.text = "Speed: \(stringSpeed)"
+        
+    }
+
+}
